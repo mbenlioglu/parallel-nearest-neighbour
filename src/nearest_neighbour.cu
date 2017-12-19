dbca4d582752f0492a82fa7030579af2ef76eb57
M.Mucahid Benlioglu <mbenlioglu@sabanciuniv.edu>
2017-12-19 23:06:28 +0300

    Kernel update

--- src/nearest_neighbour.cu
+++ src/nearest_neighbour.cu
@@ -87,7 +87,7 @@ cudaError_t CudaNearestNeighbour(Point *train, Point *test, int *result, int tra
 	// Launch a kernel on the GPU with one thread for each element, and check for errors.
 	//
 	printf("%d, %d\n", testSize, testSize / numThreads);
-	NearestNeighbourKernel<<<(testSize+THREADS_PER_BLOCK-1)/THREADS_PER_BLOCK, dimBlock>>>(dev_result, dev_rowPtr, dev_colInd, testSize);
+	NearestNeighbourKernel<<<(testSize+THREADS_PER_BLOCK-1)/THREADS_PER_BLOCK, dimBlock>>>(dev_train, dev_test, dev_result, trainSize, testSize);
 
 	//===========================================================================================================================
 	// Check for any errors launching the kernel
